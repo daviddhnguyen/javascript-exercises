75cecadae9a57a773c68640c5bc4beb76ac59356
David DH Nguyen <51886638+daviddhnguyen@users.noreply.github.com>
2024-01-19 09:12:13 -0800

    Solves TOP JS Foundations 4 Assignment 7 - untested

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+function roundOneDecimal(num) {
+  return Math.round(num *10)/10
+}
+
+const convertToCelsius = function(tempInF) {
+  return roundOneDecimal((tempInF-32)*(5/9));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempInC) {
+  return roundOneDecimal(tempInC*(9/5)+32);
 };
 
 // Do not edit below this line
