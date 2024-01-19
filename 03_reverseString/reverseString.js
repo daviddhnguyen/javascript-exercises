b49977105031b5aa8c2843e9b0c0cf0897adaa15
David Nguyen <51886638+daviddhnguyen@users.noreply.github.com>
2024-01-18 20:52:59 -0800

    Solve TOP JS Foundations 4 assignment 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(text) {
+    stringLength = text.length-1;
 
+    let reverseText = '';
+    for (i = stringLength; i >= 0; i--) {
+        reverseText = reverseText.concat(text.charAt(i));
+    }
+    return reverseText;
 };
 
 // Do not edit below this line
