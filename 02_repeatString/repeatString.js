38d12224e9395933891e516da503f56efd056b97
David Nguyen <51886638+daviddhnguyen@users.noreply.github.com>
2024-01-18 20:32:58 -0800

    Solves TOP JS Foundation 4 Assignment 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function(text, num) {
-    
+const repeatString = function(text,numOfRepeats) {
+    let repeatingText = ''
+    if (numOfRepeats < 0) {
+        return 'ERROR';
+    }
+
+    for (i = 0; i < numOfRepeats; i++) {
+      repeatingText = repeatingText.concat(text);
+    }
+    return repeatingText;
 };
 
 // Do not edit below this line
