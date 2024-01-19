a9968a2f464ee13d2ba5b56c6a1ce1a023f02cf5
David Nguyen <51886638+daviddhnguyen@users.noreply.github.com>
2024-01-18 22:11:40 -0800

    Solves TOP JS Foundations 4 Assignment 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
+const sumAll = function(num1,num2) {
+    if (typeof num1 !== typeof num2 || num1 < 0 || num2 < 0) {
+        return "ERROR";
+    }
+    const bigNum = Math.max(num1, num2);
+    const smallNum = Math.min(num1, num2);
+    let sum = 0;
 
+    for (i = smallNum; i <= bigNum; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
