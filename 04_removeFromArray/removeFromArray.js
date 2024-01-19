564795562d16c863bafb22c0a4eaf726806cd749
David Nguyen <51886638+daviddhnguyen@users.noreply.github.com>
2024-01-18 21:33:38 -0800

    Solves TOP JS foundations 4 assignment 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array) {
+    const argsLength = arguments.length;
+    
+    let newArray = array;
 
+    for (i = 1; i < argsLength; i++) {
+        console.log(arguments[i]);
+        newArray = newArray.filter((element) => element !== arguments[i]);
+    }
+    return newArray;
 };
 
 // Do not edit below this line
