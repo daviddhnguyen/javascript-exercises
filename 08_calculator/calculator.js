c32e1971fe6351494967c4079038fa413cd4619a
David Nguyen <51886638+daviddhnguyen@users.noreply.github.com>
2024-01-27 09:28:18 -0800

    Completes exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(a) {
+	let sum = 0;
+  for (let i = 0; i < a.length; i++) {
+    sum += a[i];
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(a) {
+  let multiply = 1;
+  for (let i = 0; i < a.length; i++) {
+    multiply *= a[i];
+  }
+  return multiply;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let factorial = 1;
+  for (let i = 1; i <= a; i++) {
+    factorial *= i;
+  }
+  return factorial;
 };
 
 // Do not edit below this line
