8c126b6cf99c84c9920336583f45dcc4b186e5a2
David Nguyen <51886638+daviddhnguyen@users.noreply.github.com>
2024-01-27 09:34:01 -0800

    Completes exercise - all tests passed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,15 @@
-const fibonacci = function() {
-
-};
+const fibonacci = function(num) {
+    if (num < 0) {
+        return "OOPS";
+    };
+    
+    let f = [0, 1];
+    
+    for (let i = 2; i <= num; i++) {
+      f.push(f[i-2]+f[i-1]);
+    };
+    return f[num];
+  };
 
 // Do not edit below this line
 module.exports = fibonacci;
