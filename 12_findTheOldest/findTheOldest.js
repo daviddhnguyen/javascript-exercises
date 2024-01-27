a5d6700c7676a5dadd459bd1d4b2692913fab9f7
David Nguyen <51886638+daviddhnguyen@users.noreply.github.com>
2024-01-27 10:06:52 -0800

    complete exercise - passed all tests

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,24 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    // calculates age and saves first person
+    // updates entry if new person is older than current oldest person
+    return people.reduce((oldest, currentPerson) => {
+        const oldestAge = getAge(
+            oldest.yearOfBirth,
+            oldest.yearOfDeath);
+        const currentAge = getAge(
+            currentPerson.yearOfBirth,
+            currentPerson.yearOfDeath
+        );
+        return oldestAge < currentAge ? currentPerson:oldest;
+    });
 };
+
+function getAge(birth, death) {
+    if (!death) {
+        death = new Date().getFullYear();
+    }
+    return death - birth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
