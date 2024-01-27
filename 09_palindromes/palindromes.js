203d7a92557e44ef69bcdf2609881a9985e8e4fc
David Nguyen <51886638+daviddhnguyen@users.noreply.github.com>
2024-01-27 09:30:33 -0800

    Completes exercise - passed all tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,17 @@
-const palindromes = function () {
-
-};
+const palindromes = function(text) {
+    const punctuation = /[\.,?!]/g;
+    let newText = text.replace(punctuation, '');
+    newText = newText.replaceAll(' ', '');
+    newText = newText.toLowerCase();
+    
+    let reverseText = '';
+    
+    for (let i = newText.length-1; i >= 0; i--) {
+      reverseText = reverseText.concat(newText[i]);
+    };
+    
+    return newText === reverseText;
+  };
 
 // Do not edit below this line
 module.exports = palindromes;
